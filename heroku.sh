60128316f180fc0014714ea21c3e56156c84c8dc
Vincent Dubois <dubois.vct@free.fr>
2015-09-13 15:06:52 +0200

    [FIX] assigning entities to current user

--- heroku.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mvn3 clean install -Pprod,fast -DskipTests
-heroku login
-heroku create --app vdubois-tracker-app
-heroku addons:create heroku-postgresql --app vdubois-tracker-app
-heroku config --app vdubois-tracker-app
-heroku deploy:jar --jar target/*.war --app vdubois-tracker-app
